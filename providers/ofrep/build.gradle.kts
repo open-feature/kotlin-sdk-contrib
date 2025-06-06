515dffa9347cb468d149af3e2a53500060f15b87
Bence Hornák <bence.hornak@gmail.com>
2025-06-06 18:42:19 +0200

    Add OfrepProvider from go-feature-flag-kotlin-provider
    Signed-off-by: Bence Hornák <bence.hornak@gmail.com>

---
+++ providers/ofrep/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    id("com.android.library") version "8.10.1"
+}
+
+kotlin {
+    androidTarget()
+}
+
+kotlin {
+    androidTarget()
+
+    sourceSets {
+        commonMain.dependencies {
+            // TODO: update to api(libs.openfeature.kotlin.sdk)
+            api("dev.openfeature:android-sdk:0.3.2")
+
+            api(libs.kotlinx.coroutines.core)
+            api(libs.okhttp)
+            // TODO: replace with multiplatform JSON library
+            api("com.google.code.gson:gson:2.12.1")
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.okhttp.mockwebserver)
+        }
+    }
+}
+
+android {
+    namespace = "dev.openfeature.kotlin.contrib.providers.ofrep"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,20 @@
 rootProject.name = "open-feature-kotlin-sdk-contrib"
 
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 dependencyResolutionManagement {
     repositories {
+        google()
         mavenLocal()
         mavenCentral()
     }
 }
 
 include(":providers:env-var")
+include(":providers:ofrep")
